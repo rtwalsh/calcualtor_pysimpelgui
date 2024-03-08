0a16ea09080965beab794436756062193297dac6
Robert Walsh <rwalsh@excalibur-solutions.com>
2024-03-08 15:45:30 -0500

    Add colors to buttons, window frame

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ DISPLAY_KEY = "DISPLAY"
 
 BLACK = "black"
 WHITE = "white"
+DARK_GRAY = "darkgray"
+WHITE_ON_RED = "white on red"
+BLACK_ON_LIGHT_GRAY = "black on lightgray"
+BLACK_ON_LIGHT_BLUE = "black on lightblue"
 
 #   FUNCTIONS
-def create_button(label):
-    button = sg.Button(label, size=BUTTON_SIZE)
+def create_button(label, color=BLACK_ON_LIGHT_GRAY):
+    button = sg.Button(label, size=BUTTON_SIZE, button_color=color)
     return button
 
 #   GLOBAL VARIABLES
@@ -37,14 +41,14 @@ display = sg.Text("",
                   background_color=WHITE)
 
 layout = [
-    [ create_button("C"), display ],
-    [ create_button("7"), create_button("8"), create_button("9"), create_button("+") ],
-    [ create_button("4"), create_button("5"), create_button("6"), create_button("-") ],
-    [ create_button("1"), create_button("2"), create_button("3"), create_button("x") ],
-    [ create_button("."), create_button("0"), create_button("="), create_button("/") ]
+    [ create_button("C", WHITE_ON_RED), display ],
+    [ create_button("7"), create_button("8"), create_button("9"), create_button("+", BLACK_ON_LIGHT_BLUE) ],
+    [ create_button("4"), create_button("5"), create_button("6"), create_button("-", BLACK_ON_LIGHT_BLUE) ],
+    [ create_button("1"), create_button("2"), create_button("3"), create_button("x", BLACK_ON_LIGHT_BLUE) ],
+    [ create_button("."), create_button("0"), create_button("=", BLACK_ON_LIGHT_BLUE), create_button("/", BLACK_ON_LIGHT_BLUE) ]
 ]
 
-window = sg.Window("Calculator", layout)
+window = sg.Window("Calculator", layout, background_color=DARK_GRAY)
 
 #   MAIN PROGRAM
 while True:
